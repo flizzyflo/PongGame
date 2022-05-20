a9b00b46b4bec3f0937d30669b0d807084965b0e
flizzyflo <vip3r1@gmx.de>
2022-05-20 18:28:23 +0200

    update game

--- main.py
+++ main.py
@@ -9,16 +9,16 @@ def main() -> None:
     root.title("Playstation 5 Spiel")
     gameboard = GameBoard(root_window= root, width= 400, height= 300, bgColour= "black")
 
-    planke = Plank(GameBoardObject= gameboard)
-    planke.create_plank()
+    plankePlayer1 = Plank(GameBoardObject= gameboard)
+    plankePlayer1.create_plank()
 
-    item = Ball(GameBoardObject = gameboard, PlankObject = planke, speed = 5)
+    item = Ball(GameBoardObject = gameboard, PlankObject = plankePlayer1, speed = 5)
     item.create_ball()
 
     gameboard.canvasItem.focus_set()
-    gameboard.canvasItem.bind("<a>", lambda event: planke.move_left())
+    gameboard.canvasItem.bind("<a>", lambda event: plankePlayer1.move_left())
     gameboard.canvasItem.bind("<s>", lambda event: item.random_start())
-    gameboard.canvasItem.bind("<d>", lambda event: planke.move_right())
+    gameboard.canvasItem.bind("<d>", lambda event: plankePlayer1.move_right())
 
     gameboard.main()
 
