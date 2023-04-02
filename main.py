08b6b5d3c03e46b61b1d16e4ec1e110cf9f0562c
flizzyflo <vip3r1@gmx.de>
2023-04-02 00:01:23 -0400

    refactoring

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from game_objects.static_objects.obstacle import Obstacle
 from settings.settings import FRAME_BORDERWIDTH, GAMEBOARD_HEIGTH, GAMEBOARD_WIDTH, TITLE
 from tkinter import Button, Label, Frame, BOTH, GROOVE
 from userinterface.gameboard import GameBoard
@@ -5,40 +6,42 @@ from userinterface.scoreboard import Scoreboard
 
 from userinterface.main_window import MainWindow
 
+
 def main() -> None:
     root = MainWindow()
     root.title(TITLE)
 
-    information_frame = Frame(master= root, 
-                              relief= GROOVE, 
-                              borderwidth= FRAME_BORDERWIDTH)
+    information_frame = Frame(master=root,
+                              relief=GROOVE,
+                              borderwidth=FRAME_BORDERWIDTH)
     information_frame.pack(fill=BOTH)
 
-    gameboard_frame = Frame(master= root, 
+    gameboard_frame = Frame(master=root,
                             relief=GROOVE, 
-                            borderwidth= FRAME_BORDERWIDTH)
+                            borderwidth=FRAME_BORDERWIDTH)
     gameboard_frame.pack()
 
-    scoreboard = Scoreboard(master= information_frame)
+    scoreboard = Scoreboard(master=information_frame)
     scoreboard.pack()
-    Label(master= information_frame, text= "Press 'A' to move left\nPress 'D' to move right").pack()
+    Label(master=information_frame,
+          text="Press 'A' to move left\nPress 'D' to move right").pack()
     
-    start_game_button = Button(master= information_frame, 
-                               text= "Start game")
-    start_game_button.pack(fill= BOTH)
+    start_game_button = Button(master=information_frame,
+                               text="Start game")
+    start_game_button.pack(fill=BOTH)
 
-    gameboard = GameBoard(master= gameboard_frame, 
-                          gameboard_width= GAMEBOARD_WIDTH, 
-                          gameboard_height= GAMEBOARD_HEIGTH, 
-                          gameboard_background_colour= "black", 
-                          scoreboard_object= scoreboard,
-                          start_button = start_game_button)
+    gameboard = GameBoard(master=gameboard_frame,
+                          gameboard_width=GAMEBOARD_WIDTH,
+                          gameboard_height=GAMEBOARD_HEIGTH,
+                          gameboard_background_colour="black",
+                          scoreboard_object=scoreboard,
+                          start_button=start_game_button)
     gameboard.pack()
     gameboard.focus_set()
     gameboard.bind("<a>", lambda event: gameboard.plank.move_left())
     gameboard.bind("<d>", lambda event: gameboard.plank.move_right())
 
-    start_game_button.config(command= lambda: gameboard.ball.random_start())
+    start_game_button.config(command=lambda: gameboard.ball.random_start())
 
     root.mainloop()
     
